008a9a81436fde00d1f6ab973a2f8f33a4279177
emmanuel budan tela <budanemmanuel@gmail.com>
2023-08-29 20:51:21 +0100

    fixed the authRoute 	modified:   routes/authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 import { register, login } from "../controllers/authController.js";
-import { checkAdmin} from "../middlewares/authMiddleWare.js";
+import { checkAdmin } from "../middlewares/authMiddleware.js";
 
 
 
